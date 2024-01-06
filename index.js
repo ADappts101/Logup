58f81c84ab7880bdae653c99350271f1005e88fa
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-01-06 18:49:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const logupbtn = document.querySelector("button.logupbtn");
-logupbtn.setAttribute("disabled");
+logupbtn.setAttribute("disabled", "disabled");
 function trueCall() {
   logupbtn.removeAttribute("disabled");
 }
 
 function falseCall() {
-  logupbtn.setAttribute("disabled");
+  logupbtn.setAttribute("disabled", "disabled");
 }
