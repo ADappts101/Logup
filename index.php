b075c98b23f257c04a562adbb741e46a41c86f49
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-01-06 16:44:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 $email=GET['email'];
 $pwd=POST['pwd'];
 $pwd-repeat=POST['pwd-repeat'];
-if ($pwd-repeat==$pwd) {
-  
+if ($pwd-repeat===$pwd) {
+  echo `<script>firstCall();</script>`;
+} else {
+  echo `<script>secondCall();</script>`;
 }
