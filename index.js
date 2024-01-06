24268e733ac474389c0ce9626eb0438ff55bbf11
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-01-06 19:24:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const logupbtn = document.querySelector("button.logupbtn");
 const rememberCheckbox = document.getElementById("rememberCheckbox");
 
-// Check if there's saved data in local storage when the page loads
 window.onload = function () {
     const savedEmail = localStorage.getItem("savedEmail");
     const savedPassword = localStorage.getItem("savedPassword");
@@ -13,16 +12,13 @@ window.onload = function () {
     }
 };
 
-// Add an event listener to the checkbox
 rememberCheckbox.addEventListener("change", function () {
     if (this.checked) {
-        // Save email and password to local storage
         const email = document.getElementsByName("email")[0].value;
         const password = document.getElementsByName("pwd")[0].value;
         localStorage.setItem("savedEmail", email);
         localStorage.setItem("savedPassword", password);
     } else {
-        // Remove saved data if the checkbox is unchecked
         localStorage.removeItem("savedEmail");
         localStorage.removeItem("savedPassword");
     }
@@ -30,9 +26,9 @@ rememberCheckbox.addEventListener("change", function () {
 
 logupbtn.setAttribute("disabled", "disabled");
 function trueCall() {
-  logupbtn.removeAttribute("disabled");
+    logupbtn.removeAttribute("disabled");
 }
 
 function falseCall() {
-  logupbtn.setAttribute("disabled", "disabled");
+    logupbtn.setAttribute("disabled", "disabled");
 }
